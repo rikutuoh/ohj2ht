4eb6106b9e150ebf2e7f72fa243fa2fd6d75148a
Tuohimetsä Riku Viljami <riku.v.tuohimetsa@student.jyu.fi>
2021-02-02 16:30:01 +0200

    Lisätty eclipse-projekti ja käyttöliittymä

---
+++ src/RTKfx/RallitietokantaGUIController.java
@@ -0,0 +1,10 @@
+package RTKfx;
+
+/**
+ * @author rikut
+ * @version 2.2.2021
+ *
+ */
+public class RallitietokantaGUIController {
+      // TODO
+}
---
+++ src/RTKfx/RallitietokantaMain.java
@@ -0,0 +1,38 @@
+package RTKfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author rikut
+ * @version 2.2.2021
+ *
+ */
+public class RallitietokantaMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("RallitietokantaGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final RallitietokantaGUIController rallitietokantaCtrl = (RallitietokantaGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("rallitietokanta.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Rallitietokanta");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
